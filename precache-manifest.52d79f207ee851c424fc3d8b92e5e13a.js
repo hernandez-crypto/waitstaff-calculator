a27f976bc626ad0efe8a7ef3b868cf77a30c6013
julio hernandez <hernandezjulio58@ymail.com>
2020-02-05 17:19:29 -0800

    Updates

---
+++ precache-manifest.52d79f207ee851c424fc3d8b92e5e13a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37feea2995a67c4974c5131b7a0b66bc",
+    "url": "/waitstaff-calculator/index.html"
+  },
+  {
+    "revision": "12a1d7f7e13709ec198d",
+    "url": "/waitstaff-calculator/static/css/main.97356bc2.chunk.css"
+  },
+  {
+    "revision": "0b88d918369096fe06ea",
+    "url": "/waitstaff-calculator/static/js/2.44076d33.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/waitstaff-calculator/static/js/2.44076d33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12a1d7f7e13709ec198d",
+    "url": "/waitstaff-calculator/static/js/main.1ed23e55.chunk.js"
+  },
+  {
+    "revision": "b53fe242c0f0ce6b7942",
+    "url": "/waitstaff-calculator/static/js/runtime-main.6965af91.js"
+  },
+  {
+    "revision": "c2b60f3be43a9ffdf992e690a2bc1f86",
+    "url": "/waitstaff-calculator/static/media/woodLightBackground.c2b60f3b.jpg"
+  }
+]);
